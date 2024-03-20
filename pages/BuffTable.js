80b0b08c0dd50ef41697b4f4059930d7cb25e490
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2024-03-20 09:44:11 +0000

    fix/remove-mongo

--- pages/BuffTable.js
+++ pages/BuffTable.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import { connectToDatabase } from '../util/mongodb';
 import styled from 'styled-components';
 import { useState, useEffect, useRef} from 'react';
 import 'dotenv/config';
